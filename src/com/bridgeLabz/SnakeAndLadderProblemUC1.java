cff95fa499c0c7f45bafd49e5e76b691cb21d66d
smsmaurya <sumeshmaurya786@gmail.com>
2023-10-23 14:15:11 +0530

    [Sumesh] Add:- Player intial position

---
+++ src/com/bridgeLabz/SnakeAndLadderProblemUC1.java
@@ -0,0 +1,9 @@
+package com.bridgeLabz;
+
+public class SnakeAndLadderProblemUC1 {
+    public static void main(String[] args) {
+        System.out.println(" Start Snake and ladder game");
+        int playerPosition = 0;
+        System.out.println("The player position from start : "+playerPosition);
+    }
+}
