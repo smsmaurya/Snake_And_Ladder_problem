9f30150db144438d97c662a45ee4fa9b147686fc
smsmaurya <sumeshmaurya786@gmail.com>
2023-10-23 13:51:59 +0530

    [Sumesh] Add:- Snake And Ladder Problem

---
+++ src/com/bridgeLabz/SnakeAndLadderProblem.java
@@ -0,0 +1,7 @@
+package com.bridgeLabz;
+
+public class SnakeAndLadderProblem {
+    public static void main(String[] args) {
+        System.out.println("Welcome in Snake and Ladder problem ......");
+    }
+}
