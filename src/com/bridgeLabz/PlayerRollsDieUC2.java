b0c07c2292278d2aa0cc5289d35b086061d8d5ad
smsmaurya <sumeshmaurya786@gmail.com>
2023-10-23 14:26:10 +0530

    [Sumesh] Add:- Player rolls the die

--- src/com/bridgeLabz/PlayerRollsDieUC2.java
+++ src/com/bridgeLabz/PlayerRollsDieUC2.java
@@ -1,2 +1,9 @@
-package com.bridgeLabz;public class PlayerRollsDieUC2 {
+package com.bridgeLabz;
+
+public class PlayerRollsDieUC2 {
+    public static void main(String[] args) {
+        System.out.println(" Start Snake and ladder game");
+        int die = (int)(Math.random()*10)%6 +1;
+        System.out.println("Player rolls the die : "+die);
+    }
 }
